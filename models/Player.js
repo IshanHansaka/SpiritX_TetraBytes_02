61bfa23ee344e1137a3c24a6877c08069f00f5d7
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-08 12:49:07 +0530

    feat: create Player schema

---
+++ models/Player.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const playerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  university: { type: String, required: true },
+  category: {
+    type: String,
+    enum: ['Batsman', 'All-Rounder', 'Bowler'],
+  },
+  totalRuns: { type: Number, default: 0 },
+  inningsPlayed: { type: Number, default: 0 },
+  wickets: { type: Number, default: 0 },
+  oversBowled: { type: Number, default: 0 },
+  runsConceded: { type: Number, default: 0 },
+});
+
+const Player = mongoose.model('Player', playerSchema);
+
+module.exports = Player;
