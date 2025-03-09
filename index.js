47f33c33ac3fcfb6837b5d6b2c5f2b20bfcf652f
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-03-09 09:59:09 +0530

    feat: add chat endpoint

--- index.js
+++ index.js
@@ -4,17 +4,25 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const healthCheck = require('./routes/healthCheck');
 const playerRoute = require('./routes/playerRoute');
 const loginRoute = require('./routes/loginRoute');
 const signupRoute = require('./routes/signupRoute');
+const chatRoutes = require('./routes/chatRoute');
 
 app.use('/', healthCheck);
 app.use('/players', playerRoute);
 app.use('/login', loginRoute);
 app.use('/signup', signupRoute);
+app.use('/chat', chatRoutes);
 
 module.exports = app;
